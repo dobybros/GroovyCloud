4c68e720a0cdf70e4fe63cd52be5b8b554136898
licklulu <lick1244036947@163.com>
2019-11-07 13:41:57 +0800

    remove deploy

--- container/src/main/java/deploy/lulu/tuition/TuitionConstants.java
+++
@@ -1,9 +0,0 @@
-package deploy.lulu.tuition;
-
-public interface TuitionConstants {
-    String PATH = "/home/lick/projects/TuitionCloud/";
-    String DOCKERNAME = "docker_tc";
-    String GRIDFSHOST = "mongodb://localhost:27017";
-    String PREFIX = "scripts";
-    String tuitionCore = PATH + "TCCore";
-}
