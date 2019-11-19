069c0bff8c7eb1b61dfa5075a52bcaa5050503be
Aplomb Chen <aplomb.chen@gmail.com>
2019-11-19 17:44:38 +0800

    update stackOverError

--- cloudcore/src/main/java/com/docker/storage/redis/RedisHandler.java
+++ cloudcore/src/main/java/com/docker/storage/redis/RedisHandler.java
@@ -137,7 +137,6 @@ public class RedisHandler {
         if (!lockedCode.equals(set(LOCK_PREFIX + id, lock, "NX", "EX", expirseTime))) {
             throw new CoreException(CoreErrorCodes.ERROR_LOCK, "Can't lock.");
         }
-        ;
         return lock;
     }
 
