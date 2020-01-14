e39050dace26e09dc264f151ab3506f1af21ffb4
胡占静 <372581643@qq.com>
2020-01-14 12:17:50 +0800

    groovy to java

---
+++ container/src/main/java/com/docker/utils/JWTUtils.java
@@ -0,0 +1,35 @@
+package com.docker.utils;
+
+import io.jsonwebtoken.JwtBuilder;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.impl.DefaultJwtBuilder;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by lick on 2019/6/27.
+ * Description：
+ */
+public class JWTUtils {
+    public static final String secretkey = "GROOVYCLOUDQILIAO";
+    public static String createToken(String key, Map<String, Object> claims, Long expireTime) throws Exception {
+        Map<String, Object> map = new HashMap<>();
+        map.put("alg", "HS256");
+        map.put("typ", "JWT");
+        JwtBuilder jwtBuilder = new DefaultJwtBuilder();
+        if(claims != null){
+            jwtBuilder.setClaims(claims);
+        }
+        if(expireTime != null){
+            jwtBuilder.setExpiration(new Date(System.currentTimeMillis() + expireTime));
+        }
+        String token = jwtBuilder
+                .setIssuedAt(new Date())
+                .signWith(SignatureAlgorithm.HS256, secretkey + key)
+                .compact();
+        return token;
+    }
+}
+
