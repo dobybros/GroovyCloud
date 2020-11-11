023e654151f70fc48f4431a2b4b6fcd7c27f6bde
胡占静 <372581643@qq.com>
2020-11-11 12:58:25 +0800

    删除测试log

--- core/src/main/java/script/groovy/runtime/GroovyRuntime.java
+++ core/src/main/java/script/groovy/runtime/GroovyRuntime.java
@@ -271,7 +271,6 @@ public class GroovyRuntime extends ScriptRuntime {
             urls.toArray(theUrls);
 //            parentClassLoader = new URLClassLoader(theUrls, parentClassLoader);
             parentClassLoader = new ParentLastURLClassLoader(theUrls);
-            LoggerEx.info(TAG, "------ use parent last url class loader ------");
         }
 
         if (runtimeBootListener != null) {
